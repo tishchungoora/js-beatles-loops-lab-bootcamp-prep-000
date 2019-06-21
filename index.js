db7510bce0e059578d3592aac1c0d8d9687eda05
Tish Chungoora <tishal82@gmail.com>
2019-06-21 22:15:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ function theBeatlesPlay(musicians, instruments) {
 // Function for facts about John Lennon
 
 function whileLoop(factsArray) {
-  
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
   while (number > 0) {
     console.log(--number);
   }
