b9f9b44400f97e4dd37f0bacf53806cd2d9c3ab5
Tish Chungoora <tishal82@gmail.com>
2019-06-21 22:02:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var musicianSentence;
   var instrumentPlayed = [];
-  for (var i = 0; i < ??; i++) {
+  for (var i = 0; i < 30; i++) {
     musicianSentence = musicians[i] + 'plays' + instruments[i];
   }
   return musicianAndInstrumentSentence;
